dce03c7d7c6b13c4b4e1290ec859e164d9c612fa
Techborn <unborntech@pm.me>
2018-07-12 19:04:57 -0500

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pIRC',
-      version='0.1',
+      version='0.5.3',
       description='A light, customizable python-based IRC bot library.',
       url='https://github.com/techborn/pIRC',
       author='Techborn',
